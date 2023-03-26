4faf2461a54f9314f2e308c4309f91576ebde6da
tv3636 <tv3636@gmail.com>
2023-03-25 22:17:03 -0700

    update thread embedding to include full thread

--- src/utils/openai-utils.ts
+++ src/utils/openai-utils.ts
@@ -38,30 +38,39 @@ export async function addEmbeddingToDocs() {
   }
 }
 
-async function saveEmbeddingForMessage(message: any, messageFile: string) {
-  let embedding = await getEmbeddingForText(message.content);
+async function saveEmbeddingForMessage(message: any, messageFile: string, overrideText?: string) {
+  let embedding = await getEmbeddingForText(overrideText ?? message.content);
   message.embedding = embedding;
   fs.writeFileSync(messageFile, JSON.stringify(message, null, 4));
-  console.log(`Saved embedding for ${message.id}`);
+  console.log(`Saved embedding for ${message.id}. \nText: ${overrideText ?? message.content}`);
+}
+
+async function getThreadForMessage(channelId: string, messageId: string, addEmbedding: boolean) {
+  let messages = '';
+  let firstMessagePath = `messages/${channelId}/${messageId}.json`;
+  let threadMessage = JSON.parse(fs.readFileSync(firstMessagePath, 'utf8'));
+  messages += threadMessage.content + "\n";
+
+  for (const threadFile of fs.readdirSync(`messages/${channelId}/${messageId}`)) {          
+    let message = JSON.parse(
+      fs.readFileSync(`messages/${channelId}/${messageId}/${threadFile}`, 'utf8')
+    );
+    messages += message.content + "\n";         
+  }
+
+  if (addEmbedding) {
+    await saveEmbeddingForMessage(threadMessage, firstMessagePath, messages);
+  };
+
+  return [threadMessage, messages];
 }
 
 export async function addEmbeddingToMessages() {
-  for (const file of fs.readdirSync('messages')) {
-    for (const channelFile of fs.readdirSync(`messages/${file}`)) {
-      if (fs.lstatSync(`messages/${file}/${channelFile}`).isDirectory()) {
-        for (const threadFile of fs.readdirSync(`messages/${file}/${channelFile}`)) {
-          let message = JSON.parse(
-            fs.readFileSync(`messages/${file}/${channelFile}/${threadFile}`, 'utf8')
-          );
-
-          await saveEmbeddingForMessage(message, `messages/${file}/${channelFile}/${threadFile}`);
-        }
-      } else {
-        let message = JSON.parse(
-          fs.readFileSync(`messages/${file}/${channelFile}`, 'utf8')
-        );
-        
-        await saveEmbeddingForMessage(message, `messages/${file}/${channelFile}`);
+  for (const channel_id of fs.readdirSync('messages')) {
+    for (const message_id of fs.readdirSync(`messages/${channel_id}`)) {
+      let path = `messages/${channel_id}/${message_id}`;
+      if (fs.lstatSync(path).isDirectory() && fs.existsSync(`${path}.json`)) {
+        await getThreadForMessage(channel_id, message_id, true);                
       }
     }
   }
@@ -85,22 +94,12 @@ export async function getBestMatchingDocs(queryEmbedding: any) {
 
 export async function getBestMatchingMessages(queryEmbedding: any) {
   let results: any = {};
-  for (const file of fs.readdirSync('messages')) {
-    for (const channelFile of fs.readdirSync(`messages/${file}`)) {
-      if (fs.lstatSync(`messages/${file}/${channelFile}`).isDirectory()) {
-        for (const threadFile of fs.readdirSync(`messages/${file}/${channelFile}`)) {
-          let fullPath = `messages/${file}/${channelFile}/${threadFile}`;
-          let message = JSON.parse(fs.readFileSync(fullPath, 'utf8'));
-
-          results[fullPath] = similarity(queryEmbedding, message.embedding);
-        }
-      } else {
-        let fullPath = `messages/${file}/${channelFile}`;
-        let message = JSON.parse(
-          fs.readFileSync(fullPath, 'utf8')
-        );
-
-        results[fullPath] = similarity(queryEmbedding, message.embedding);
+  for (const channel_id of fs.readdirSync('messages')) {
+    for (const message_id of fs.readdirSync(`messages/${channel_id}`)) {
+      let path = `messages/${channel_id}/${message_id}`;
+      if (fs.lstatSync(path).isDirectory() && fs.existsSync(`${path}.json`)) {
+        let [thread, threadText] = await getThreadForMessage(channel_id, message_id, false);
+        results[path] = similarity(queryEmbedding, thread.embedding);
       }
     }
   }
@@ -114,33 +113,34 @@ export async function getResponseForQuery(query: string) {
   if (enc.encode(query).length <= MAX_TOKENS) {
     let queryEmbedding = await getEmbeddingForText(query);
 
-    // create a string of the top 5 docs
+    // create a string of the top 3 docs
     let docString = "";
-    (await getBestMatchingDocs(queryEmbedding)).slice(0, 5).map((result) => {
+    (await getBestMatchingDocs(queryEmbedding)).slice(0, 3).map((result) => {
       console.log(result);
       docString += JSON.parse(fs.readFileSync(`docs/${result.file}`, 'utf8')).doc.body + "\n";
+    });  
+
+    let messageString = "";
+    (await getBestMatchingMessages(queryEmbedding)).slice(0,15).map(async (result) => {
+      console.log(result);
+      let path = result.file.split('/');      
+      let [thread, threadText] = await getThreadForMessage(path[1], path[2], false);
+      messageString += threadText + "\n";
+
+      console.log(threadText);
     });
 
-    //console.log(docString);
-
-    /*
     const completion = await openai.createChatCompletion({
       model: 'gpt-4',
       messages: [
         {"role": "system", "content": "You are a friendly and helpful user support agent for the Reservoir API. You use the Reservoir API docs to answer user questions."},
         {"role": "user", "content": "Here are some relevant docs to help answer user questions: \n" + docString},
-        {"role": "user", "content": query}
+        {"role": "user", "content": "You can also use the following support chat history to help answer the question: \n" + messageString},
+        {"role": "user", "content": "The user's question is: " + query}
       ]
     })
 
     console.log(completion.data.choices[0].message);
-
-    */
-
-    (await getBestMatchingMessages(queryEmbedding)).slice(0,100).map((result) => {
-      console.log(result);
-      console.log(JSON.parse(fs.readFileSync(result.file, 'utf8')).content);
-    });
   }
 }
 
